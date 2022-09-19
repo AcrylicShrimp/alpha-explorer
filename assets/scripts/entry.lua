254bf880a8ad4cbb3f82d089723f0992d35f1bdf
AcrylicShrimp <led789zxpp@naver.com>
2022-09-19 14:16:56 +0900

    script: convert to new API.

--- assets/scripts/entry.lua
+++ assets/scripts/entry.lua
@@ -1,19 +1,3 @@
-local audio1 = mk.asset.load_audio_clip("sfx/jump")
-local audio2 = mk.asset.load_audio_clip("bgm/BabyElephantWalk60")
-
-local source1 = mk.entity.EntityBuilder.new()
-  :audio_source({})
-  :build()
-local source2 = mk.entity.EntityBuilder.new()
-  :audio_source({})
-  :build()
-
-source1.audio_source.clip = audio1
-source1.audio_source:play()
-
-source2.audio_source.clip = audio2
-source2.audio_source:play()
-
 local camera = mk.entity.EntityBuilder.new()
   :name("camera")
   :camera({
@@ -39,105 +23,91 @@ mk.entity.EntityBuilder.new()
 
 require("assets/scripts/utils/fps-counter")
 
--- local generate_map = require("assets/scripts/map-gen/generate")
--- local map = generate_map(100, 100)
+local generate_map = require("assets/scripts/map-gen/generate")
+local map = generate_map(100, 100)
 
--- mk.Entity.build {
---   name = "map",
---   alpha_tilemap_renderer = {
---     order = 0,
---     fore_shader = mk.Shader.load("glyph"),
---     back_shader = mk.Shader.load("color"),
---     font = mk.Font.load("Courier Prime Sans"),
---     font_size = 16,
---     thickness = 0.5,
---     smoothness = 2 / 16,
---     tilemap = {
---       tile_width = 16,
---       tile_height = 16,
---       tile_count_x = 100,
---       tile_count_y = 100,
---       layer = map.map,
---       tileset = {
---         tiles = {
---           {
---             fore_color = mk.Color.black(),
---             back_color = mk.Color.white(),
---             character = "#"
---           },
---           {
---             fore_color = mk.Color.rgba(1, 1, 1, 0.5),
---             back_color = mk.Color.black(),
---             character = "."
---           }
---         }
---       }
---     }
---   }
--- }
+mk.entity.EntityBuilder.new()
+  :name("map")
+  :alpha_tilemap_renderer({
+    layer = mk.render.Layer.new(1),
+    order = 0,
+    color = mk.render.Color.white(),
+    fore_shader = mk.asset.load_shader("glyph"),
+    back_shader = mk.asset.load_shader("color"),
+    font = mk.asset.load_font("Courier Prime Sans"),
+    font_size = 16,
+    thickness = 0.5,
+    smoothness = 2 / 16,
+    tilemap = mk.render.AlphaTilemap.new(
+      16, 16,
+      100, 100,
+      map.map,
+      mk.render.AlphaTileset.new({
+        mk.render.AlphaTile.new(mk.render.Color.black(), mk.render.Color.white(), "#"),
+        mk.render.AlphaTile.new(mk.render.Color.from_rgba(1, 1, 1, 0.5), mk.render.Color.black(), "."),
+      })
+    ),
+  })
+  :build()
 
--- local player = mk.Entity.build {
---   name = "player",
---   size = {
---     width = 16,
---     height = 16
---   },
---   glyph_renderer = {
---     order = 1,
---     color = mk.Color.rgb(0 / 255, 150 / 255, 105 / 255),
---     shader = mk.Shader.load("glyph"),
---     font = mk.Font.load("Courier Prime Sans"),
---     font_size = 16,
---     thickness = 0.5,
---     smoothness = 2 / 16,
---     text = "@",
---     config = {
---       horizontal_align = "center",
---       vertical_align = "middle"
---     }
---   }
--- }
+local player = mk.entity.EntityBuilder.new()
+  :name("player")
+  :size(mk.structure.Size.new(16, 16))
+  :glyph_renderer({
+    layer = mk.render.Layer.new(1),
+    order = 1,
+    color = mk.render.Color.from_rgb(0 / 255, 150 / 255, 105 / 255),
+    shader = mk.asset.load_shader("glyph"),
+    font = mk.asset.load_font("Courier Prime Sans"),
+    font_size = 16,
+    thickness = 0.5,
+    smoothness = 2 / 16,
+    text = "@",
+    config = mk.glyph.GlyphLayoutConfig.new(
+      mk.glyph.HorizontalAlign.Center,
+      mk.glyph.VerticalAlign.Middle,
+      mk.glyph.WrapStyle.Word,
+      true
+    ),
+  })
+  :build()
 
--- local keys = {}
+player.transform.position = mk.structure.Vec2.new(
+  (map.rooms[1].x + map.rooms[1].width / 2) * 16,
+  ((100 - map.rooms[1].y) - map.rooms[1].height / 2) * 16
+)
 
--- mk.Event.KeyDown.listen(function(event)
---   keys[event.key] = true
+camera.transform.parent = player.transform
 
---   if event.key == "space" then
---     source1.audio_source:play()
---   end
--- end);
+local keys = {}
 
--- mk.Event.KeyUp.listen(function(event)
---   keys[event.key] = false
--- end);
+mk.event.KeyDown.listen(function(event)
+  keys[event.key] = true
+end);
 
--- mk.Event.PostUpdate.listen(function(event)
---   if not keys["left"] and not keys["right"] and not keys["up"] and not keys["down"] then
---     return
---   end
+mk.event.KeyUp.listen(function(event)
+  keys[event.key] = false
+end);
 
---   local position = player.transform.position
+mk.event.PostUpdate.listen(function(event)
+  if not keys["left"] and not keys["right"] and not keys["up"] and not keys["down"] then
+    return
+  end
 
---   if keys["left"] then
---     position.x = position.x - event.dt * 400
---   end
---   if keys["right"] then
---     position.x = position.x + event.dt * 400
---   end
---   if keys["up"] then
---     position.y = position.y + event.dt * 400
---   end
---   if keys["down"] then
---     position.y = position.y - event.dt * 400
---   end
+  local position = player.transform.position
 
---   player.transform.position = position
--- end)
+  if keys["left"] then
+    position.x = position.x - event.dt * 400
+  end
+  if keys["right"] then
+    position.x = position.x + event.dt * 400
+  end
+  if keys["up"] then
+    position.y = position.y + event.dt * 400
+  end
+  if keys["down"] then
+    position.y = position.y - event.dt * 400
+  end
 
--- camera.transform.parent = player.transform
-
--- player.transform.position = {
---   x = (map.rooms[1].x + map.rooms[1].width / 2) * 16,
---   y = ((100 - map.rooms[1].y) - map.rooms[1].height / 2) * 16
--- }
+  player.transform.position = position
+end)
